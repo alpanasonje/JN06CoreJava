ecdd1ee08176d32a2483d783555e669b23aa9338
alpana <sonje.alpana@gmail.com>
2024-02-09 17:36:35 +0530

    commit

---
+++ JN06CoreJava/src/com/tnsif/dayone/Demo.java
@@ -0,0 +1,14 @@
+package com.tnsif.dayone;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Students!!");
+		Demo d=new Demo();
+		d.main();
+	}
+	void main()
+	{
+		System.out.println("This is my own main function");
+	}
+}
